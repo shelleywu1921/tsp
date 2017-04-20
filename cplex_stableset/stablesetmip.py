9f35818e3702f144ac637f7cecb11425571cfde2
Shelley Wu <shelleywu1921@gmail.com>
2017-04-20 11:02:02 -0400

    working on construct_model

--- cplex_stableset/stablesetmip.py
+++ cplex_stableset/stablesetmip.py
@@ -1,8 +1,22 @@
-# give a graph
+# give a graph graph
 
 import sys
 import cplex
 from cplex.exceptions import CplexError
 import networkx
+
+
+# max weight stable set
+def construct_model(graph):
+    obj=[]
+    ub=[]
+    lb=[]
+    
+    ctype=['B' for node in graph.nodes()]
+    colnames=['x'+str(node) for node in Graph.nodes()]
+    rhs=[1.0 for edge in graph.edges()]
+    rownames=['r'+str(i) for i in len(graph.edges())]
+    sense=''.join(['L' for i in len(graph.edges())])
+
 
 
