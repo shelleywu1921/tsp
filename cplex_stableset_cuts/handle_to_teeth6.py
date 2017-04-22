33b6e81fb9054f7dd20d2f72d0a727e8718b227f
Shelley Wu <shelleywu1921@gmail.com>
2017-04-21 20:26:05 -0400

    bug fix

--- cplex_stableset_cuts/handle_to_teeth6.py
+++ cplex_stableset_cuts/handle_to_teeth6.py
@@ -61,55 +61,55 @@ def find_comb2(teeth_pool,handle_pool):
 	for handle in handle_pool:
 		xdH=handle_pool[handle]['xds']
 		handleset=handle_pool[handle]['cutset']
+		newfile.write('Handle Number: %d \n' % handle)
+		newfile.write('Number of vertices in the handle: %d \n' % len(handle_pool[handle]['cutset']))
+		newfile.write('Handle Set: '+repr(handle_pool[handle]['cutset']) + '\n')
+
+		print('Handle number: %d' % handle)
+		print('Number of nodes in the handle: %d' % len(handle_pool[handle]['cutset']))
 
 		if len(handleset) <= 2:
-			newfile.write('Handle Number: %d\n' % handle)
 			newfile.write('Handle too small, discarded \n\n')
-			print('Handle Number: %d' %handle)
 			print('Handle too small, discarded')
 
 		else: 
 			eligible_teeth=find_all_teeth2(teeth_pool, handleset)
-			newfile.write('Handle Number: %d \n' % handle)
 			newfile.write('Number of eligible_teeth: %d \n' % len(eligible_teeth['nodes']))
-			newfile.write('Handle Set: '+repr(handle_pool[handle]['cutset']) + '\n')
 			newfile.write('eligible_teeth: ' +repr([tooth for tooth, xdT in eligible_teeth['nodes']]) +'\n\n')
 			
-			print('Handle Number: %d' %handle)
 			print('Number of eligible_teeth: %d' % len(eligible_teeth['nodes']))
 
 			if len(eligible_teeth['nodes']) >=3:
 				prob = populate_odd_weighted_stableset(eligible_teeth,xdH,eps)
 
-				if prob != None:
+				if prob != None and prob.solution.pool.get_num() >=1 :
 					handle_counter +=1
 					comb_counter = comb_counter + prob.solution.pool.get_num()
 
-				    for i in range(prob.solution.pool.get_num()):
-				        odd_teeth=[eligible_teeth['nodes'][j][0] for j ,x in enumerate(prob.solution.pool.get_values(i)[:-1]) if x ==1.0]
-				        num_of_teeth=len(odd_teeth)
-				        sum_xdT = sum(eligible_teeth['nodes'][j][1] for j,x in enumerate(prob.solution.pool.get_values(i)[:-1]) if x == 1.0)
-				        comb_surplus = xdH+sum_xdT-3*num_of_teeth
+					for i in range(prob.solution.pool.get_num()):
+						odd_teeth=[eligible_teeth['nodes'][j][0] for j ,x in enumerate(prob.solution.pool.get_values(i)[:-1]) if x ==1.0]
+						num_of_teeth=len(odd_teeth)
+						sum_xdT = sum(eligible_teeth['nodes'][j][1] for j,x in enumerate(prob.solution.pool.get_values(i)[:-1]) if x == 1.0)
+						comb_surplus = xdH+sum_xdT-3*num_of_teeth
+						newfile.write('Set of Teeth: \n'+ repr(odd_teeth))
+						newfile.write('{0:<20}{1:<20}{2:<20}{3:<20}\n'.format('NumofTeeth', 'x(delta(H))', 'sum x(delta(Ti))', 'CombSurp'))
+						newfile.write('{0:<20}{1:<20}{2:<20}{3:<20}\n\n'.format(num_of_teeth ,xdH, sum_xdT , comb_surplus))
 
-				        newfile.write('Set of Teeth: \n'+ repr(odd_teeth))
-				        newfile.write('{0:<20}{1:<20}{2:<20}{3:<20}\n'.format('NumofTeeth', 'x(delta(H))', 'sum x(delta(Ti))', 'CombSurp'))
-				        newfile.write('{0:<20}{1:<20}{2:<20}{3:<20}\n\n'.format(num_of_teeth ,xdH, sum_xdT , comb_surplus))
-
-				    newfile.write('\n')
-				    newfile.write('Total number of violated combs for this handle: %d \n\n' % prob.solution.pool.get_num())
+					newfile.write('\n')
+					newfile.write('Total number of violated combs for this handle: %d \n\n' % prob.solution.pool.get_num())
 
 
-				'''
-				num_viol_combs=populate_odd_weighted_stableset(eligible_teeth,xdH,eps)
-				if num_viol_combs != None:
-					handle_counter +=1
-					comb_counter = comb_counter + num_viol_combs
-					print('More violated combs found!!!')
-					print('Number of handles that has a violated comb so far: %d' % handle_counter)
-					print('Number of violated combs found so far: %d' % comb_counter)
-				'''
+					'''
+					num_viol_combs=populate_odd_weighted_stableset(eligible_teeth,xdH,eps)
+					if num_viol_combs != None:
+						handle_counter +=1
+						comb_counter = comb_counter + num_viol_combs
+						print('More violated combs found!!!')
+						print('Number of handles that has a violated comb so far: %d' % handle_counter)
+						print('Number of violated combs found so far: %d' % comb_counter)
+					'''
 
-				else:
+				else: 
 					newfile.write('No violated comb using this handle is found. \n\n')
 					print('No violated comb using this handle is found.')
 			else:
@@ -119,6 +119,10 @@ def find_comb2(teeth_pool,handle_pool):
 	newfile.write('In summary: \n')
 	newfile.write('Number of handles with violated combs: %d \n' % handle_counter)
 	newfile.write('Total number of violated combs found: %d \n' % comb_counter)
+	
+	print('In summary:')
+	print('Number of handles with violated combs: %d' % handle_counter)
+	print('Total number of violated combs found: %d' % comb_counter)
 
 	return None
 
